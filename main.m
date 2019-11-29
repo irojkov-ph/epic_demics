474ad4285985adedc807a42057818d87cc15313d
Ivan Rojkov <irojkov@student.ethz.ch>
2019-11-29 19:57:42 +0100

    main

--- main.m
+++ main.m
@@ -1,19 +1,18 @@
 addpath('src/');
 
-%%
-% <<<<<<< NICO
-% config.zero = 0;
-% config.mu = 0.0001;
-config.nb_cell = 80;
-config.nb_decision_step = 200;
-config.todraw = ["state_density";"vaccination_density";"state"];
-%config.beta = 4.8;
-config.r_ill = -10;
-config.r_vacc = -4;
-default_config.alpha = 1/(4*6);
-config.p_zero_plus_neighbours = false;
-config.patient_zero_coord = [40,40];
-start(config);
+%% <<<<<<< NICO
+% % config.zero = 0;
+% % config.mu = 0.0001;
+% config.nb_cell = 80;
+% config.nb_decision_step = 200;
+% config.todraw = ["state_density";"vaccination_density";"state"];
+% %config.beta = 4.8;
+% config.r_ill = -10;
+% config.r_vacc = -4;
+% default_config.alpha = 1/(4*6);
+% config.p_zero_plus_neighbours = false;
+% config.patient_zero_coord = [40,40];
+% start(config);
 
 % for i=1:20
 %     try
@@ -37,27 +36,31 @@ start(config);
 % end
 
 %% <<<<<<< IVAN
-% config.zero = 0;
-% config.mu = 0.0001;
-config.name = 'test';
-config.nb_cell = 80;
-config.nb_decision_step = 200;
-config.todraw = ["state";"distance_from_patient_zero"];
-config.dynamic = true;
-config.patient_zero_coord = [40,40];
-config.p_zero_plus_neighbours = false;
+% clear config
+% config.name = 'test';
+% n = 100;
+% config.nb_cell = n;
+% config.nb_decision_step = 500;
+% config.patient_zero_coord = [n/2,n/2];
+% config.p_zero_plus_neighbours = false;
+% config.beta = 1;
+% config.gamma = 2;
+% config.todraw = ["state";"max_area_infection";"distance_from_patient_zero"];
+% 
+% start(config);
 
-start(config);
 
-
-%% Variation of beta
-for i=11:20
+%% Variation of beta (1 to 10)
+clear config
+for i=1:101
     try
         config.name = 'var_beta';
         config.nb_cell = 100;
-        config.nb_decision_step = 500;
-        config.beta = 0.05*(i-1);
-        config.todraw = ["state_density";"state";"vaccination_density";"mean_age"];
+        config.nb_decision_step = 200;
+        config.patient_zero_coord = [50,50];
+        config.p_zero_plus_neighbours = false;
+        config.beta = (i-1)*0.1;
+        config.todraw = ["state_density";"state";"vaccination_density";"max_area_infection";"distance_from_patient_zero"];
         start(config);
     catch ME
         global epic_demics_path system
@@ -74,15 +77,45 @@ for i=11:20
     end
 end
 
+%% Variation of nb_cell (6 to 180 )
+N = [6:6:42,44:2:100,102:6:150];
+clear config
+for i=1:45
+    try
+        config.name = 'var_nb_cell';
+        config.nb_cell = N(i);
+        config.nb_decision_step = 200;
+        config.patient_zero_coord = [N(i)/2,N(i)/2];
+        config.p_zero_plus_neighbours = false;
+        config.beta = 1;
+        config.todraw = ["state_density";"state";"vaccination_density";"max_area_infection";"distance_from_patient_zero"];
+        start(config);
+    catch ME
+        global epic_demics_path system
+        name = num2str(round(posixtime(datetime('now'))));
+        save([epic_demics_path,filesep,'logs',filesep,config.name,name,'_system_error.mat'],'system');
+        
+        % Write in logs
+        fileID = fopen([epic_demics_path,filesep,'logs',filesep,'errors.txt'],'a+');
+        fprintf(fileID,['\n ERROR in simulation ',config.name,name,'! Here is the log of the error: \n',...
+                        ME.identifier,'\n',ME.message,'\n']);
+        fclose(fileID);
+        
+        close all;
+    end
+end
+clear N
+
 
 %% Long SIR w/o vaccination w/o dynamic
+clear config
 try
     config.name = 'SIR_no_vacc_no_dyn';
     config.nb_cell = 100;
-    config.nb_decision_step = 500;
+    config.nb_decision_step = 300;
     config.vaccination = false;
     config.dynamic = false;
-    config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age"];
+    config.todraw = ["state";"vaccination_density";"state_density"];
     start(config);
 catch ME
     global epic_demics_path system
@@ -99,13 +132,14 @@ catch ME
 end
 
 %% Long SIR w vaccination w/o dynamic
+clear config
 try
     config.name = 'SIR_yes_vacc_no_dyn';
     config.nb_cell = 100;
-    config.nb_decision_step = 500;
+    config.nb_decision_step = 300;
     config.vaccination = true;
     config.dynamic = false;
-    config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age"];
+    config.todraw = ["state";"vaccination_density";"state_density"];
     start(config);
 catch ME
     global epic_demics_path system
@@ -122,13 +156,14 @@ catch ME
 end
 
 %% Long SIR w/o vaccination w dynamic
+clear config
 try
     config.name = 'SIR_no_vacc_yes_dyn';
     config.nb_cell = 100;
-    config.nb_decision_step = 500;
+    config.nb_decision_step = 300;
     config.vaccination = false;
     config.dynamic = true;
-    config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age"];
+    config.todraw = ["state";"vaccination_density";"state_density"];
     start(config);
 catch ME
     global epic_demics_path system
@@ -145,110 +180,14 @@ catch ME
 end
 
 %% Long SIR w vaccination w dynamic
+clear config
 try
     config.name = 'SIR_yes_vacc_yes_dyn';
     config.nb_cell = 100;
-    config.nb_decision_step = 500;
+    config.nb_decision_step = 300;
     config.vaccination = true;
     config.dynamic = true;
-    config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age"];
-    start(config);
-catch ME
-    global epic_demics_path system
-    name = num2str(round(posixtime(datetime('now'))));
-    save([epic_demics_path,filesep,'logs',filesep,config.name,name,'_system_error.mat'],'system');
-
-    % Write in logs
-    fileID = fopen([epic_demics_path,filesep,'logs',filesep,'errors.txt'],'a+');
-    fprintf(fileID,['\n ERROR in simulation ',config.name,name,'! Here is the log of the error: \n',...
-                    ME.identifier,'\n',ME.message,'\n']);
-    fclose(fileID);
-
-    close all;
-end
-
-
-%% Long SIR w/o vaccination w/o dynamic w patient zero in the middle
-try
-    config.name = 'SIR_no_vacc_no_dyn_w_PZ';
-    config.nb_cell = 100;
-    config.nb_decision_step = 500;
-    config.vaccination = false;
-    config.dynamic = false;
-    config.patient_zero_coord = [50,50];
-    config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age";"age"];
-    start(config);
-catch ME
-    global epic_demics_path system
-    name = num2str(round(posixtime(datetime('now'))));
-    save([epic_demics_path,filesep,'logs',filesep,config.name,name,'_system_error.mat'],'system');
-
-    % Write in logs
-    fileID = fopen([epic_demics_path,filesep,'logs',filesep,'errors.txt'],'a+');
-    fprintf(fileID,['\n ERROR in simulation ',config.name,name,'! Here is the log of the error: \n',...
-                    ME.identifier,'\n',ME.message,'\n']);
-    fclose(fileID);
-
-    close all;
-end
-
-%% Long SIR w vaccination w/o dynamic w patient zero in the middle
-try
-    config.name = 'SIR_yes_vacc_no_dyn_w_PZ';
-    config.nb_cell = 100;
-    config.nb_decision_step = 500;
-    config.vaccination = true;
-    config.dynamic = false;
-    config.patient_zero_coord = [50,50];
-    config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age";"age"];
-    start(config);
-catch ME
-    global epic_demics_path system
-    name = num2str(round(posixtime(datetime('now'))));
-    save([epic_demics_path,filesep,'logs',filesep,config.name,name,'_system_error.mat'],'system');
-
-    % Write in logs
-    fileID = fopen([epic_demics_path,filesep,'logs',filesep,'errors.txt'],'a+');
-    fprintf(fileID,['\n ERROR in simulation ',config.name,name,'! Here is the log of the error: \n',...
-                    ME.identifier,'\n',ME.message,'\n']);
-    fclose(fileID);
-
-    close all;
-end
-
-%% Long SIR w/o vaccination w dynamic w patient zero in the middle
-try
-    config.name = 'SIR_no_vacc_yes_dyn_w_PZ';
-    config.nb_cell = 100;
-    config.nb_decision_step = 500;
-    config.vaccination = false;
-    config.dynamic = true;
-    config.patient_zero_coord = [50,50];
-    config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age";"age"];
-    start(config);
-catch ME
-    global epic_demics_path system
-    name = num2str(round(posixtime(datetime('now'))));
-    save([epic_demics_path,filesep,'logs',filesep,config.name,name,'_system_error.mat'],'system');
-
-    % Write in logs
-    fileID = fopen([epic_demics_path,filesep,'logs',filesep,'errors.txt'],'a+');
-    fprintf(fileID,['\n ERROR in simulation ',config.name,name,'! Here is the log of the error: \n',...
-                    ME.identifier,'\n',ME.message,'\n']);
-    fclose(fileID);
-
-    close all;
-end
-
-%% Long SIR w vaccination w dynamic w patient zero in the middle
-try
-    config.name = 'SIR_yes_vacc_yes_dyn_w_PZ';
-    config.nb_cell = 100;
-    config.nb_decision_step = 500;
-    config.vaccination = true;
-    config.dynamic = true;
-    config.patient_zero_coord = [50,50];
-    config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age";"age"];
+    config.todraw = ["state";"vaccination_density";"state_density"];
     start(config);
 catch ME
     global epic_demics_path system
@@ -265,25 +204,25 @@ catch ME
 end
 
 %% Variation of alpha
-for i=1:20
-    try
-        config.name = 'var_alpha';
-        config.nb_cell = 100;
-        config.nb_decision_step = 500;
-        config.alpha = 0.05*(i-1);
-        config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age"];
-        start(config);
-    catch ME
-        global epic_demics_path system
-        name = num2str(round(posixtime(datetime('now'))));
-        save([epic_demics_path,filesep,'logs',filesep,config.name,name,'_system_error.mat'],'system');
-        
-        % Write in logs
-        fileID = fopen([epic_demics_path,filesep,'logs',config.name,filesep,'errors.txt'],'a+');
-        fprintf(fileID,['\n ERROR in simulation ',config.name,name,'! Here is the log of the error: \n',...
-                        ME.identifier,'\n',ME.message,'\n']);
-        fclose(fileID);
-        
-        close all;
-    end
-end
+% for i=1:20
+%     try
+%         config.name = 'var_alpha';
+%         config.nb_cell = 100;
+%         config.nb_decision_step = 500;
+%         config.alpha = 0.05*(i-1);
+%         config.todraw = ["state";"max_area_infection";"vaccination_density";"state_density";"mean_age"];
+%         start(config);
+%     catch ME
+%         global epic_demics_path system
+%         name = num2str(round(posixtime(datetime('now'))));
+%         save([epic_demics_path,filesep,'logs',filesep,config.name,name,'_system_error.mat'],'system');
+%         
+%         % Write in logs
+%         fileID = fopen([epic_demics_path,filesep,'logs',config.name,filesep,'errors.txt'],'a+');
+%         fprintf(fileID,['\n ERROR in simulation ',config.name,name,'! Here is the log of the error: \n',...
+%                         ME.identifier,'\n',ME.message,'\n']);
+%         fclose(fileID);
+%         
+%         close all;
+%     end
+% end
